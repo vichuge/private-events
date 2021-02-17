25b632b7132011a113f6d395b3aad9ea4487f024
vichuge <victor.hugo.pacheco.flores@gmail.com>
2021-02-17 16:13:52 -0600

    Fixing details in models

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,5 +3,5 @@ class Event < ApplicationRecord
   belongs_to :creator, class_name: 'User'
 
   has_many :invitations, foreign_key: :attended_event
-  has_many :users, through => :invitations
+  #has_many :users, :through => :invitations
 end

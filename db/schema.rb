732ecb159b95db0630db2dc2942ee946c24300b3
icebox827 <icebox827@gmail.com>
2021-02-17 12:03:55 -0500

    Create event model and run migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_17_162123) do
+ActiveRecord::Schema.define(version: 2021_02_17_170307) do
+
+  create_table "events", force: :cascade do |t|
+    t.string "name"
+    t.string "location"
+    t.text "description"
+    t.datetime "date"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"

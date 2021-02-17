dece80b74eb6fdecdd5509e0225c8a96c578f7a9
vichuge <victor.hugo.pacheco.flores@gmail.com>
2021-02-17 10:49:13 -0600

    Starting session module (create)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,28 @@
 class UsersController < ApplicationController
   def show
+    @user = User.find(params[:id])
   end
 
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.create(sign_up_params)
+    if @user.save
+      flash[:notice] = 'User created successfully'
+      session[:user_id] = @user.id
+      session[:username] = @user.name
+      redirect_to user_path(@user)
+    else
+      flash[:alert] = 'User not created, please try again'
+      render 'new'
+    end
+  end
+
+  private
+
+  def sign_up_params
+    params.require(:user).permit(:name)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   #get 'users/user'
   resources :users, only: %i[new create show]
+  resources :sessions, only: %i[new create destroy]
 end

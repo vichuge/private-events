eebf8120e75d8604e125ea4824e6d4793b8d6442
icebox827 <icebox827@gmail.com>
2021-02-17 12:57:37 -0500

    Create events form

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,29 @@
 class EventsController < ApplicationController
-  def index
+  def index; end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = current_user.events.build(event_params)
+
+    if @event.save
+      flash[:notice] = 'Event created successfully'
+      redirect_to events_path
+    else
+      flash[:alert] = 'Event is not created, try again'
+      render 'new'
+    end
+  end
+
+  def show
+    @event = Event.find(params[:id])
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:name, :description, :location, :date)
   end
 end

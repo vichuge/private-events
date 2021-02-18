a92154dee732979e14cba0e070601ab0815a9cff
vichuge <victor.hugo.pacheco.flores@gmail.com>
2021-02-18 10:41:58 -0600

    Separating events after/before

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,7 @@
 class UsersController < ApplicationController
   def show
     # @user = User.find(params[:id])
-    @events = Event.where(creator_id: session[:user_id].to_i)
-  
+    @events = Event.where(creator_id: session[:user_id].to_i).order(date: :asc)
   end
 
   def new

ffd5eebc54a5e8c39da611947cdab309733c3497
vichuge <victor.hugo.pacheco.flores@gmail.com>
2021-02-18 12:09:12 -0600

    Fixing sign out

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   resources :users, only: %i[new create show]
   resources :sessions, only: %i[new create destroy]
 
-  scope :session do
-    get '/users/sign_out' => 'sessions#destroy'
+  scope :sessions do
+    get 'sign_out' => 'sessions#destroy'
   end
 end

510d80c72bc4ff767f9891a0b193734585d7747b
vichuge <victor.hugo.pacheco.flores@gmail.com>
2021-02-18 11:19:56 -0600

    Starting solution with log out

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,8 @@ Rails.application.routes.draw do
   resources :events, only: %i[index new create show]
   resources :users, only: %i[new create show]
   resources :sessions, only: %i[new create destroy]
+
+  scope :session do
+    get '/users/sign_out' => 'sessions#destroy'
+  end
 end

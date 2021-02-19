fd838e8fec25094d3e9ca20ed6df4cbb69878d29
icebox827 <icebox827@gmail.com>
2021-02-19 10:13:54 -0500

    Update models

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # get 'events/index'
   # get 'users/user'
   root 'events#index'
-  resources :events, only: %i[index new create show]
+  resources :events, only: %i[index new create show], to: 'show'
   resources :users, only: %i[new create show]
   resources :sessions, only: %i[new create destroy]
 
